7f5e609c8bdf3aea680e41761e32e37edbee9063
htk0575 <htk008kr@naver.com>
2025-08-01 13:47:56 +0900

    [feat] ASP.NET 추가 (WebAssembly)

---
+++ MiniGamePortal/MiniGamePortal/wwwroot/unityLoader.js
@@ -0,0 +1,16 @@
+﻿//유니티 로딩(WebGL은 JS기반)
+
+window.loadUnityGame = function (gamePath, canvasID) {
+    //기존 유니티 인스턴스가 있다면 제거
+    if (window.unityGameInstance) {
+        window.unityGameInstance.Quit().then(() => {
+            console.log("Previous Unity instance quit successfully.");
+            window.unityGameInstance = null;
+        }).catch((message) => {
+            console.error("Error quitting previous Unity instance:", message);
+        });
+    }
+
+    // Unity 로더 스크립트 동적으로 로드 (이미 로드된 경우 건너뛰기)
+    
+};
